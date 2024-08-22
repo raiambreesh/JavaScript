b4927c3f81f072f641f701a94b6c34c70cf8c8d9
Ambreesh Rai <118298021+raiambreesh@users.noreply.github.com>
2024-08-22 09:47:00 +0530

    variable

---
+++ 1 basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1445533
+let accountEmail = "ambreesh7@gmail.com"
+var accountPassword = "12345"
+acoountCity = " varanasi "
+
+// accountId = 2  // not allowed 
+
+accountEmail = " amb@gmail.com"
+accountPassword= "23456"
+acoountCity = "jalandhar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, acoountCity])
