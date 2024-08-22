0e34ac6c8a90e77e5ebb4fedbeca57fc74a7178f
Ambreesh Rai <118298021+raiambreesh@users.noreply.github.com>
2024-08-22 10:07:57 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3+3) // we are using nodejs , not browser
+
+let name = " ambreesh"
+let age = 22
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint 
+//string => ""
+// boolean => true / false
+// null => standalone value
+//undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof "Ambreesh");
+console.log(typeof age);
+console.log(typeof null);       // o/p object 
+console.log(typeof undefined);  // o/p undefined 
+
+
+
