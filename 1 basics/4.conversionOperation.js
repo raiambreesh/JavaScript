6e198dd242c13bd135bc40774be247d3fa76be55
Ambreesh Rai <118298021+raiambreesh@users.noreply.github.com>
2024-08-30 00:49:14 +0530

    conversion

--- 1 basics/4.conversionOperation.js
+++ 1 basics/4.conversionOperation.js
@@ -21,4 +21,33 @@ let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
+//////////////// operations //////////////
+
+let value = 3
+let negValue = - value 
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " ambreesh"
+
+let str3 = str1+ str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+console.log(true);
+console.log(+true);
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
+
+
+
 
