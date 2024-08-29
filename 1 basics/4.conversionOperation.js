da18be2c66c19cbdf0b6e975c2fc145b6911aad7
Ambreesh Rai <118298021+raiambreesh@users.noreply.github.com>
2024-08-30 00:35:29 +0530

    conversion

---
+++ 1 basics/4.conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+
+console.log(typeof score);
+
+let scores = "33"
+console.log(typeof scores);
+
+
+let valueInNumber = Number(score)
+console.log(typeof scores);
+
+let scoress = null
+console.log(typeof scoress);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//TRUE => 1; FALSE=> 0
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
